a5f49865eb20c9a476f4b61fc22e897d9ba32954
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-11-09 14:15:52 -0500

    gathering all data needed from api

---
+++ javascript.js
@@ -0,0 +1,23 @@
+async function getMainStuff() {
+  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=651b36dd9ec2d5156544989665f507bd', { mode: 'cors' });
+  const data = await response.json();
+  const currentWeatherData = {
+    lat: data.coord.lat,
+    long: data.coord.lon,
+    mainWeatherCondition: data.weather[0].main,
+    weatherDescription: data.weather[0].description,
+    temp: data.main.temp,
+    feelsLike: data.main.feels_like,
+    tempMax: data.main.temp_max,
+    tempMin: data.main.temp_min,
+    humidity: data.main.humidity,
+    pressure: data.main.pressure,
+    location: data.name,
+    country: data.sys.country,
+    windSpeed: data.wind.speed,
+  };
+  /* console.log(`data is temp:${temp}  feels like: ${feelsLike} max temp:${tempMax} min
+  temp:${tempMin} humidity:${humidity} pressure: ${pressure}`);
+   console.log(`the coordinates are ${lat},${long}`); */
+}
+getMainStuff();
